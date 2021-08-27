0efe3e5635c87e38aef5decb3aec88f6d8806f0a
Kyle Tully <kjtully@yahoo.com>
2021-08-27 13:56:28 -0500

    Using ClientInterceptor to inject W3C context

---
+++ src/main/java/com/kjt/OtelClientInterceptor.java
@@ -0,0 +1,40 @@
+package com.kjt;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.api.OpenTelemetry;
+
+public class OtelClientInterceptor implements ClientInterceptor {
+	
+	private static OpenTelemetry openTelemetry;
+	
+	public void setOpenTelemetry(OpenTelemetry openTelemetry) {
+		this.openTelemetry = openTelemetry;
+	}
+	
+	public OpenTelemetry getOpenTelemetry() {
+		return this.openTelemetry;
+	}
+
+	@Override
+	public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method,
+			CallOptions callOptions, Channel next) {
+		return new ForwardingClientCall.SimpleForwardingClientCall<>(next.newCall(method, callOptions)) {
+			@Override
+			public void start(Listener<RespT> responseListener, Metadata headers) {
+				// Inject the request with the current context
+				openTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), headers,
+						OtelUtils.getSetter());
+				// Perform the gRPC request
+				super.start(responseListener, headers);
+			}
+		};
+	}
+
+}
